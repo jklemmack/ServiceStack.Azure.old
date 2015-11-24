9dfa2ae21ee79b58bb2e84cf66080750ed10e5d5
Johann Klemmack <jklemmack@obrc.com>
2015-11-24 12:18:36 -0800

    In-progress TableCache

--- ServiceStack.Azure.Storage/TableCache.cs
+++ ServiceStack.Azure.Storage/TableCache.cs
@@ -4,32 +4,95 @@ using System;
 using System.Text;
 using System.Threading.Tasks;
 using ServiceStack.Caching;
+using ServiceStack.Logging;
+using Microsoft.WindowsAzure.Storage;
+using ServiceStack.Support;
+using ServiceStack.Text;
+using Microsoft.WindowsAzure.Storage.Table;
+using ServiceStack.DataAnnotations;
 
 namespace ServiceStack.Azure.Storage
 {
-    public class TableCache : ICacheClientExtended
+    public class AzureTableCacheClient : AdapterBase, ICacheClient, ICacheClientExtended
     {
+        static readonly DateTime EXPIRY_DATE = new DateTime(2100, 1, 1);
+
+        TableCacheEntry CreateTableEntry(string rowKey, string data = null,
+            DateTime? created = null, DateTime? expires = null)
+        {
+            var createdDate = created ?? DateTime.UtcNow;
+            return new TableCacheEntry(rowKey)
+            {
+                Data = data,
+                ExpiryDate = expires,
+                CreatedDate = createdDate,
+                ModifiedDate = createdDate,
+            };
+        }
+
+
+        protected override ILog Log { get { return LogManager.GetLogger(GetType()); } }
+        public bool FlushOnDispose { get; set; }
+
+        string connectionString;
+        string partitionKey = "";
+        CloudTable table = null;
+
+        public AzureTableCacheClient(string ConnectionString, string tableName = "Cache")
+        {
+            connectionString = ConnectionString;
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(connectionString);
+            CloudTableClient tableClient = storageAccount.CreateCloudTableClient();
+            table = tableClient.GetTableReference(tableName);
+            table.CreateIfNotExists();
+        }
+
+
+        private bool TryGetValue(string key, out TableCacheEntry entry)
+        {
+            entry = null;
+
+            TableOperation op = TableOperation.Retrieve<TableCacheEntry>(partitionKey, key);
+            TableResult retrievedResult = table.Execute(op);
+
+            if (retrievedResult.Result != null)
+            {
+                entry = retrievedResult.Result as TableCacheEntry;
+                return true;
+            }
+            else
+                return false;
+        }
+
+
+        public void Dispose()
+        {
+            if (!FlushOnDispose) return;
+
+            FlushAll();
+        }
+
+
         public bool Add<T>(string key, T value)
         {
-            throw new NotImplementedException();
+            return Add<T>(key, value, EXPIRY_DATE);
         }
 
         public bool Add<T>(string key, T value, TimeSpan expiresIn)
         {
-            throw new NotImplementedException();
+            return Add<T>(key, value, DateTime.UtcNow.Add(expiresIn));
         }
 
         public bool Add<T>(string key, T value, DateTime expiresAt)
         {
-            throw new NotImplementedException();
+            string sVal = JsonSerializer.SerializeToString<T>(value);
+            TableCacheEntry entry = CreateTableEntry(key, sVal, null, expiresAt);
+            TableOperation op = TableOperation.Insert(entry);
+            TableResult result = table.Execute(op);
+            return result.HttpStatusCode == 200;    // ???
         }
 
         public long Decrement(string key, uint amount)
-        {
-            throw new NotImplementedException();
-        }
-
-        public void Dispose()
         {
             throw new NotImplementedException();
         }
@@ -41,20 +104,15 @@ namespace ServiceStack.Azure.Storage
 
         public T Get<T>(string key)
         {
-            throw new NotImplementedException();
+            TableCacheEntry entry = null;
+            if (TryGetValue(key, out entry))
+            {
+                return JsonSerializer.DeserializeFromString<T>(entry.Data);
+            }
+            return default(T);
         }
 
         public IDictionary<string, T> GetAll<T>(IEnumerable<string> keys)
-        {
-            throw new NotImplementedException();
-        }
-
-        public IEnumerable<string> GetKeysByPattern(string pattern)
-        {
-            throw new NotImplementedException();
-        }
-
-        public TimeSpan? GetTimeToLive(string key)
         {
             throw new NotImplementedException();
         }
@@ -66,7 +124,11 @@ namespace ServiceStack.Azure.Storage
 
         public bool Remove(string key)
         {
-            throw new NotImplementedException();
+            TableCacheEntry entry = CreateTableEntry(key);
+            entry.ETag = "*";
+            TableOperation op = TableOperation.Delete(entry);
+            TableResult result = table.Execute(op);
+            return result.HttpStatusCode == 200;
         }
 
         public void RemoveAll(IEnumerable<string> keys)
@@ -76,37 +138,95 @@ namespace ServiceStack.Azure.Storage
 
         public bool Replace<T>(string key, T value)
         {
-            throw new NotImplementedException();
+            return Replace(key, value, EXPIRY_DATE);
         }
 
         public bool Replace<T>(string key, T value, TimeSpan expiresIn)
         {
-            throw new NotImplementedException();
+            return Replace(key, value, DateTime.UtcNow.Add(expiresIn));
         }
 
         public bool Replace<T>(string key, T value, DateTime expiresAt)
         {
-            throw new NotImplementedException();
+            string sVal = JsonSerializer.SerializeToString<T>(value);
+            TableCacheEntry entry = null;
+            if (TryGetValue(key, out entry))
+            {
+                entry = CreateTableEntry(key, sVal, null, expiresAt);
+                TableOperation op = TableOperation.Replace(entry);
+                TableResult result = table.Execute(op);
+                return result.HttpStatusCode == 200;
+            }
+            return false;
         }
 
         public bool Set<T>(string key, T value)
         {
-            throw new NotImplementedException();
+            return Set(key, value, EXPIRY_DATE);
         }
 
         public bool Set<T>(string key, T value, TimeSpan expiresIn)
         {
-            throw new NotImplementedException();
+            return Set(key, value, DateTime.UtcNow.Add(expiresIn));
         }
 
         public bool Set<T>(string key, T value, DateTime expiresAt)
         {
-            throw new NotImplementedException();
+            string sVal = JsonSerializer.SerializeToString<T>(value);
+            TableCacheEntry entry = CreateTableEntry(key, sVal, null, expiresAt);
+            TableOperation op = TableOperation.InsertOrReplace(entry);
+            TableResult result = table.Execute(op);
+            return result.HttpStatusCode == 200;    // ???
         }
 
         public void SetAll<T>(IDictionary<string, T> values)
         {
             throw new NotImplementedException();
+        }
+
+        public TimeSpan? GetTimeToLive(string key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> GetKeysByPattern(string pattern)
+        {
+            // Construct the query operation for all customer entities where PartitionKey="Smith".
+            TableQuery<TableCacheEntry> query = new TableQuery<TableCacheEntry>()
+                ;
+
+            return table.ExecuteQuery<TableCacheEntry>(query)
+                .Select(q => q.RowKey);
+        }
+
+        public class TableCacheEntry : TableEntity
+        {
+
+            public TableCacheEntry(string key)
+            {
+                this.PartitionKey = "";
+                this.RowKey = key;
+            }
+
+            public TableCacheEntry() { }
+
+
+            [StringLength(1024 * 2014 /* 1 MB max */
+                - 1024 /* partition key max size*/
+                - 1024 /* row key max size */
+                - 64   /* timestamp size */
+                - 64 * 3 /* 3 datetime fields */
+
+                // - 8 * 1024 /* ID */
+                )]
+
+            public string Data { get; set; }
+
+            public DateTime? ExpiryDate { get; set; }
+
+            public DateTime CreatedDate { get; set; }
+
+            public DateTime ModifiedDate { get; set; }
         }
     }
 }
